e45452f3beddba53329b45c13d3f2381cb986947
ivan <ivaninhust@126.com>
2023-11-07 10:59:06 +0800

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,99 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path, { resolve } from 'path';
+
+import manifest from './manifest';
+import makeManifest from './utils/plugins/make-manifest';
+import customDynamicImport from './utils/plugins/custom-dynamic-import';
+import addHmr from './utils/plugins/add-hmr';
+import watchRebuild from './utils/plugins/watch-rebuild';
+
+const rootDir = resolve(__dirname);
+const srcDir = resolve(rootDir, 'src');
+const pagesDir = resolve(srcDir, 'pages');
+const assetsDir = resolve(srcDir, 'assets');
+const outDir = resolve(rootDir, 'dist');
+const publicDir = resolve(rootDir, 'public');
+
+const isDev = process.env.__DEV__ === 'true';
+const isProduction = !isDev;
+
+const enableHmrInBackgroundScript = true;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@root': rootDir,
+      '@src': srcDir,
+      '@pages': pagesDir,
+      '@assets': assetsDir,
+    },
+  },
+  plugins: [
+    react(),
+    makeManifest(manifest, {
+      isDev,
+      contentScriptCssKey: regenerateCacheInvalidationKey(),
+    }),
+    customDynamicImport(),
+    addHmr({
+      background: enableHmrInBackgroundScript,
+      view: true,
+    }),
+    watchRebuild(),
+  ],
+  publicDir,
+  build: {
+    outDir,
+    /** Can slowDown build speed. */
+    // sourcemap: isDev,
+    minify: isProduction,
+    modulePreload: false,
+    reportCompressedSize: isProduction,
+    rollupOptions: {
+      input: {
+        devtools: resolve(pagesDir, 'devtools', 'index.html'),
+        panel: resolve(pagesDir, 'panel', 'index.html'),
+        content: resolve(pagesDir, 'content', 'index.ts'),
+        background: resolve(pagesDir, 'background', 'index.ts'),
+        contentStyle: resolve(pagesDir, 'content', 'style.pcss'),
+        popup: resolve(pagesDir, 'popup', 'index.html'),
+        newtab: resolve(pagesDir, 'newtab', 'index.html'),
+        options: resolve(pagesDir, 'options', 'index.html'),
+      },
+      output: {
+        entryFileNames: 'src/pages/[name]/index.js',
+        chunkFileNames: isDev
+          ? 'assets/js/[name].js'
+          : 'assets/js/[name].[hash].js',
+        assetFileNames: (assetInfo) => {
+          const { dir, name: _name } = path.parse(assetInfo.name);
+          const assetFolder = dir.split('/').at(-1);
+          const name = assetFolder + firstUpperCase(_name);
+          console.log(111, dir, _name, name, assetInfo.name, assetInfo.type);
+
+          if (name === 'Style') {
+            return `assets/css/contentStyle${cacheInvalidationKey}.chunk.css`;
+          }
+          return `assets/[ext]/${name}.chunk.[ext]`;
+        },
+      },
+    },
+  },
 });
+
+function firstUpperCase(str: string) {
+  const firstAlphabet = new RegExp(/( |^)[a-z]/, 'g');
+  return str.toLowerCase().replace(firstAlphabet, (L) => L.toUpperCase());
+}
+
+let cacheInvalidationKey: string = generateKey();
+function regenerateCacheInvalidationKey() {
+  cacheInvalidationKey = generateKey();
+  return cacheInvalidationKey;
+}
+
+function generateKey(): string {
+  return `${(Date.now() / 100).toFixed()}`;
+}
